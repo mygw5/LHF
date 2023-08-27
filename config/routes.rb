9bf4d39527595fbc363cc0529452c1b0c9f00aec
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-08-27 13:31:03 +0000

    Fix controller,devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,43 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users,skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
-
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-    sessions: "admin/sessions"
-  }
-
-  scope module: :public do
-    root to: "homes#top"
-    resources :hobbies do
-      resources :comments,  only: [:create, :destroy]
-      resources :favorites, only: [:index, :create, :destroy]
-    end
-    resources :users, only: [:show, :edit, :update] do
-      get "confirm_withdraw"
-      patch "withdraw"
-    end
-    resources :searches, only: [:index]
-    resources :tags, only: [:show] do
-      resources :post_tags, only: [:create, :destroy]
-    end
-    resources :groups, only: [:new, :index, :create, :edit, :update, :show] do
-      resources :group_users, only: [:create, :destroy]
-      resources :chats, only: [:create]
-    end
-  end
-
-  namespace :admin do
-   root to: "homes#top"
-   resources :hobbies, only: [:index, :show, :edit, :update, :destroy] do
-     resources :comments, only: [:destroy]
-   end
-   resources :tags,    only: [:index, :create, :edit, :update]
-   resources :users,   only: [:show, :edit, :update]
-   resources :groups, only: [:index, :show, :edit, :update] do
-     resources :chats, only: [:create]
-   end
-  end
-
+    
+  get 'groups/index'
+  get 'groups/new'
+  get 'groups/edit'
+  get 'groups/show'
+  get 'tags/show'
+  get 'tags/index'
+  get 'tags/edit'
+  get 'searches/index'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/confirm_withdraw'
+  get 'hobbies/new'
+  get 'hobbies/index'
+  get 'hobbies/show'
+  get 'hobbies/edit'
+  get 'homes/top'
+  devise_for :users
+  
+  
 end
