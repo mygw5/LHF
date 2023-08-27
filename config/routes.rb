edeae135986b3ed7427ec314c49bb863789dba2b
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-08-27 13:48:52 +0000

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-    
+
   get 'groups/index'
   get 'groups/new'
   get 'groups/edit'
@@ -8,16 +8,29 @@ Rails.application.routes.draw do
   get 'tags/index'
   get 'tags/edit'
   get 'searches/index'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/confirm_withdraw'
-  get 'hobbies/new'
-  get 'hobbies/index'
-  get 'hobbies/show'
-  get 'hobbies/edit'
-  get 'homes/top'
+
+
   devise_for :users
-  
-  
+
+  root to: "homes#top"
+  resources :hobbies do
+    resources :comments,  only: [:create, :destroy]
+    resources :favorites, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:index,:show, :edit, :update] do
+    get   "confirm_withdraw"
+    patch "withdraw"
+  end
+
+  resources :searches, only: [:index]
+
+  resources :tags, only: [:index, :create, :show, :edit, :update] do
+    resources :post_tags, :only: [:create, :destroy]
+  end
+
+  resources :groups, only: [:new, :create, :index, :show, :edit, :update] do
+    resources :group_users, only: [:create, :destroy]
+    resources :chats, only: [:create]
+  end
 end
