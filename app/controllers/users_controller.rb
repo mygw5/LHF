6af646991169deb60a73e7a1e510c62ec7330244
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-08-28 06:57:28 +0000

    Add mypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
+  before_action :if_not_admin, only: [:index]
+
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
@@ -10,6 +14,17 @@ class UsersController < ApplicationController
 
   def confirm_withdraw
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :hobby, :image, :is_deleted)
+  end
+
+  def if_not_admin
+    redirect_to root_path unless current_user.admin?
+  end
+
 
 
 end
