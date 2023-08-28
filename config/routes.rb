ed615f7f48faadff97b818699e06a8bd2189a7f8
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-08-28 04:46:32 +0000

    Add guest-login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  devise_scope :user do
+    post 'users/guest_sign_in', to: "users/sessions#guest_sign_in"
+  end
+
   root to: "homes#top"
   resources :post_hobbies do
     get "drafts"
@@ -25,4 +29,5 @@ Rails.application.routes.draw do
     resources :group_users, only: [:create, :destroy]
     resources :chats, only: [:create]
   end
+
 end
